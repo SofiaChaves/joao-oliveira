d1ce66bce09664756588c8c64accbb50e54608a8
Sofia Chaves <sofiachaves.ps@outlook.com>
2022-06-06 01:29:38 +0100

    Avatar Animation

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,24 +2,21 @@ import SubTitle from './SubTitle';
 
 export function Avatar() {
     return (
-        <div className="flex justify-center items-center relative">
+        <div className="flex justify-center items-center relative cursor-pointer hover:scale-105 transition-all">
             <div className="h-64 2xl:h-96 w-64 2xl:w-96 rounded-full hidden lg:block overflow-hidden">
+                {/* Image */}
                 <img src="joao.jpg" alt="Joao" className="w-full h-full" />
-                <div className="absolute inset-0 z-10 -m-20">
+                {/* Get To know Me */}
+                <div className="absolute inset-0 z-10 -m-8 hover:animate-spin-slow select-none">
                     <div className="w-full h-full font-bold uppercase">
                         <svg viewBox="0 0 100 100">
-                            <defs>
-                                <path
-                                    id="circle"
-                                    d="
-                                M 50, 50
-                                m -37, 0
-                                a 37,37 0 1,1 74,0
-                                a 37,37 0 1,1 -74,0"
-                                />
-                            </defs>
+                            <path
+                                fill="#ff000000"
+                                id="circle"
+                                d="M0,50A37,37 0 1 0100,50A37,37 0 1 00,50M50,50"
+                            />
                             <text>
-                                <textPath href="#circle" font-size="8">
+                                <textPath href="#circle" font-size="7">
                                     Get To Know me
                                 </textPath>
                             </text>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                'spin-slow': 'spin 8s linear infinite',
+            },
+        },
     },
     plugins: [require('tailwindcss-debug-screens')],
 };
